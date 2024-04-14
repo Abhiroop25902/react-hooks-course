648ad03c24cdd9e7dfb4091d0b2066c02921a97f
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-04-14 20:41:04 +0530

    Working with Component Trees

--- src/App.js
+++ src/App.js
@@ -1,17 +1,19 @@
 import './App.css';
-import {useState} from "react";
+import {FaStar} from "react-icons/fa";
+
+
+const createArray = (length) => [...Array(length)]
+
+function Star({selected = false}){
+  return <FaStar color={selected?"red":"gray"}/>;
+}
+
+function StarRating({totalStars = 5}){
+  return createArray(totalStars).map((x,index) => (<Star index={index} key={index}/>))
+}
 
 function App() {
-  const [checked, setChecked] = useState(false);
-
-    return (
-    <div className="App">
-      <input type="checkbox" value={checked} onChange={(e) => {
-        setChecked(e.target.checked)
-      }}/>
-      <p>{checked? 'checked': 'not checked'}</p>
-    </div>
-  );
+  return <StarRating totalStars={10}/>;
 }
 
 export default App;
