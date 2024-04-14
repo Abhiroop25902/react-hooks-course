7bc896e64ac7ef200b9d2e6b7b6e8c12ad1a8441
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-04-14 20:49:46 +0530

    Sending Interactions up the component tree

--- src/App.js
+++ src/App.js
@@ -1,19 +1,32 @@
 import './App.css';
 import {FaStar} from "react-icons/fa";
+import {useState} from "react";
 
 
 const createArray = (length) => [...Array(length)]
 
-function Star({selected = false}){
-  return <FaStar color={selected?"red":"gray"}/>;
+function Star({selected = false, onSelect}) {
+    return <FaStar color={selected ? "red" : "gray"} onClick={onSelect}/>;
 }
 
-function StarRating({totalStars = 5}){
-  return createArray(totalStars).map((x,index) => (<Star index={index} key={index}/>))
+function StarRating({totalStars = 5}) {
+    const [selectedStars, setSelectedStars] = useState(0);
+    return <>
+        {createArray(totalStars).map(
+            (x, i) => (
+                <Star
+                    key={i}
+                    selected={selectedStars > i}
+                    onSelect={() => setSelectedStars(i + 1)}/>
+            )
+        )}
+        <p>{selectedStars} of {totalStars}</p>
+    </>
+
 }
 
 function App() {
-  return <StarRating totalStars={10}/>;
+    return <StarRating totalStars={4}/>;
 }
 
 export default App;
