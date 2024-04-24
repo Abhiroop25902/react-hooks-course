9b644a46154648fffa7724999443b742bea5f31d
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-04-24 20:04:22 +0530

    Working with the dependency array

--- src/App.js
+++ src/App.js
@@ -3,14 +3,21 @@ import {useEffect, useState} from "react";
 
 function App() {
     const [name, setName] = useState('Jan');
+    const [admin, setAdmin] = useState(false);
 
     useEffect(() => {
-        document.title = `Celebrate ${name}`
-    });
+        console.log(`Celebrate ${name}`);
+    }, [name]);
+
+    useEffect(() => {
+        console.log(`The user is: ${admin ? "admin" : "not admin"}`);
+    }, [admin])
 
     return <section>
         <p>Congratulations {name}</p>
         <button onClick={() => setName('Will')}>Change Winner</button>
+        <p>{admin ? "logged in" : 'Not logged in'}</p>
+        <button onClick={() => setAdmin(true)}>Log In</button>
     </section>
 }
 
