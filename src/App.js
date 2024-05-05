72ade9d7d1caf3b2db7e11b4aef247d0044265b8
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-05-05 13:17:49 +0530

    Building a fetch component

--- src/App.js
+++ src/App.js
@@ -1,19 +1,23 @@
 import './App.css';
-import {useTrees} from "./index";
+import {useFetch} from "./useFetch";
 
-function App() {
-    const {trees} = useTrees();
+function App({login}) {
+    const {data, loading, error} = useFetch(`https://api.github.com/users/${login}`);
+
+    if (loading) return <h1>Loading...</h1>
+    if (error) return <pre>{JSON.stringify(error, null, 2)}</pre>;
 
     return (
         <>
-            <h1>Trees that I have heard of</h1>
-            <ul>
-                {
-                    trees.map(tree => <li key={tree.id}>{tree.type}</li>)
-                }
-            </ul>
+            <img src={data.avatar_url} alt={data.login}/>
+
+            <h1>{data.login}</h1>
+            {data.name && <p>{data.name}</p>}
+            {data.location && <p>{data.location}</p>}
+
         </>
     )
+
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -6,6 +6,6 @@ import App from './App';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
-        <App/>
+        <App login={"Abhiroop25902"}/>
     </React.StrictMode>
 );
