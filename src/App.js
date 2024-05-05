e630a30d3b8cc21132f0ed840fa2c7fa6e51644a
Abhiroop25902 <abhiroop.m25902@gmail.com>
2024-05-05 12:49:04 +0530

    Reusing form logic with custom hooks

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import './App.css';
-import {useState} from "react";
+import {useInput} from "./useInput";
 
 function App() {
-    const [sound, setSound] = useState("");
-    const [color, setColor] = useState("#000000");
+    const [soundProps, resetSound] = useInput("");
+    const [colorProps, resetColor] = useInput("#000000");
 
     const submit = (e) => {
         e.preventDefault();
-        alert(`${sound} sounds like ${color}`);
-        setColor("000000");
-        setSound('')
+        alert(`${soundProps.value} sounds like ${colorProps.value}`);
+        resetColor();
+        resetSound();
     }
 
     return (
 
         <form onSubmit={submit}>
-            <input value={sound} onChange={e => setSound(e.target.value)} type="text" placeholder={'Sound..'}/>
-            <input value={color} onChange={e => setColor(e.target.value)} type="color"/>
+            <input {...soundProps} type="text" placeholder={'Sound..'}/>
+            <input {...colorProps} type="color"/>
             <button>Add</button>
         </form>
 
